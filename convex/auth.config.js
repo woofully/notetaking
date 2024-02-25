3fa0e6955137c8f7aa5ea405f3f3fbda290d3b55
Louis <louis@bonduet.com>
2024-02-25 14:34:58 +0800

    bbb

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,8 +1,8 @@
 export default {
   providers: [
     {
-      domain: "https://actual-glider-21.clerk.accounts.dev",
+      domain: "https://smart-skylark-16.clerk.accounts.dev",
       applicationID: "convex",
-    }
-  ]
-}
+    },
+  ],
+};
